6afec5104e2e6ddcd15e2d3fc7a201cc390342f3
Dylan Mikus <dbmikus@gmail.com>
2014-02-11 23:03:47 -0500

    We run through iterations of our EM and update the individual word translation probabilities

--- hw1/ibm_model1.py
+++ hw1/ibm_model1.py
@@ -8,6 +8,9 @@ def prepare_iters(bitext):
     f_source_expect = defaultdict(float)
 
     for (f, e) in bitext:
+        # We include the null token as an option for a word translation in the
+        # source language.
+        f.insert(0, '')
         e_count = defaultdict(int)
         f_count = defaultdict(int)
         for e_i in e:
@@ -16,33 +19,39 @@ def prepare_iters(bitext):
             f_count[f_j] += 1
 
         for (f_j, cf) in f_count.items():
-            # Determining the number of times that f_j is the source of any
-            # translation
-            # TODO if we include the null string, remove the + 1 from
-            # the denominator
-            # f_source_expect[f_j] += (float(f_count[f_j]) / (len(f) + 1)) * len(e)
-
-            # Determining the expected number of times f_j translates to e_i in
-            # sentence pair (f, e)
-            for (e_i, ce) in e_count.items():
-                trans_prob = (float(f_count[f_j]) / (len(f) + 1)) * e_count[e_i]
+             for (e_i, ce) in e_count.items():
+                # Determining the expected number of times f_j translates to
+                # e_i in sentence pair (f, e)
+                trans_prob = (float(f_count[f_j]) / (len(f))) * e_count[e_i]
                 f_e_expect[(f_j, e_i)] += trans_prob
+                # Determining the number of times that f_j is the source of any
+                # translation
                 f_source_expect[f_j] += trans_prob
 
 
+    # computes the probabilities for all p(e_i | f_j)
+    return compute_pef_probs(f_e_expect, f_source_expect)
+
+
+def compute_pef_probs(f_e_expect, f_source_expect):
+    """
+    Computes all of the probabilities for translations: p(e_i | f_j)
+    """
     p_e_f = defaultdict(int)
     for ((f_j, e_i), trans_exp) in f_e_expect.items():
-        p_e_f[(f_j, e_i)] = f_e_expect[(f_j, e_i)] / f_source_expect[f_j]
+        p_e_f[(f_j, e_i)] = f_e_expect[(f_j, e_i)] /  f_source_expect[f_j]
 
     return p_e_f
 
 
-def ibm_model1(bitext, me_iters, f_count, e_count, fe_count):
+def ibm_model1(bitext, me_iters):
     # p(e | f, m): this is a dictionary that stores probabilities for
-    # translations of (e_i,f_j) pairs
+    # translations of (e_i,f_j) pairs.
+    # This is a the probability for the translation of sentences.
     p_e_fm = defaultdict(int)
 
     # p(e_i, f_j): translation parameters.
+    # This is the probability for the translation of specific words.
     # We use an EM algorithm to develop this.
     # We have random variables:
     #   f_e = number of times source word f aligns to target word e
@@ -52,8 +61,26 @@ def ibm_model1(bitext, me_iters, f_count, e_count, fe_count):
     # p_e_f = defaultdict(lambda k: 1.0 / float(len(fe_count.keys())))
 
     for i in xrange(me_iters):
+        # The expected number of times we translate from a given word f_j to a
+        # given word e_i.
+        f_e_expect = defaultdict(int)
+        f_source_expect = defaultdict(int)
         for (f, e) in bitext:
-            (p_e_fm, p_e_f) = prob_pair(p_e_fm, p_e_f, f, e)
+            (f_e_prob, f_e_expct, f_source_expct) = prob_pair(p_e_f, f, e)
+            p_e_fm[(" ".join(f), " ".join(e))] = f_e_prob
+
+            # updating the expected values across the whole corpus
+            for ((f_j, e_i), trans_exp) in f_e_expct.items():
+                f_e_expect[(f_j, e_i)] += trans_exp
+            for (f_j, trans_exp) in f_source_expct.items():
+                f_source_expect[f_j] += trans_exp
+
+        # Updating the probabilities for translations between words
+        p_e_f = compute_pef_probs(f_e_expect, f_source_expect)
+
+    print p_e_f
+
+    return "formatted alignments expected here"
 
 
 # For each i in [1, m] (that is, for each empty word spot in the target
@@ -63,7 +90,7 @@ def ibm_model1(bitext, me_iters, f_count, e_count, fe_count):
 # f_{a_i} will translate to.
 #
 # me_iters is the number of ME iterations we do to improve our probabilities.
-def prob_pair(p_e_fm, p_e_f, f, e):
+def prob_pair(p_e_f, f, e):
     # add the null token as an empty string
     f.insert(0, '')
 
@@ -76,22 +103,40 @@ def prob_pair(p_e_fm, p_e_f, f, e):
     # picks out a position in the target sentence to translate to.
     a_i_prob = 1.0 / float(n)
 
+    # The probability we generate is:
+    # p(e | f, m) = \prod_{i=1}^m  \sum_{j=0}^n  p(a_i = j) x p(e_i | f_j)
+
+    prob_prod = 1
+
+    # Expected number of translations from some f_j to some e_i
+    f_e_expect = defaultdict(int)
+    f_source_expect = defaultdict(int)
+
     # for each i \in [1,2, ..., m]
     for (i, e_i) in enumerate(e):
+        # We must compute the sum of probabilities for translating from all
+        # words f_j1, f_j2, etc. in the source sentence f, to word e_i in the
+        # target sentence e.
+        prob_sum = 0
         for (j, f_j) in enumerate(f):
+            # Computing the expected values that we use for our iterative EM.
+            # These expected values are used to give us the new p(e_i | f_j)
+            # probabilities at the end of each iteration.
+            f_e_expect[(f_j, e_i)] += p_e_f[(f_j, e_i)]
+            f_source_expect[f_j] += p_e_f[(f_j, e_i)]
+
+
+            # Computing the probability relevant to the probabalistic expression
+            # we care about in the end.
+            # That is, for target sentence e and source sentence f and target
+            # sentence length m:
+            #   p(e | f, m)
+
             # p(a_i = j): alignment probability
             p_ai_j = a_i_prob
 
-            # Get the expected number of times that f translates into e
-            # throughout the whole corpus.
-            # This is p(e | f) * number of possible (e, f) pairs in sentences
-            if (f_j, e_i) not in p_e_f:
+            prob_sum += p_ai_j * p_e_f[(f_j, e_i)]
 
+        prob_prod *= prob_sum
 
-                f_to_e_prob = 1.0 / fe_count[(f_j, e_i)]
-                # expected number of times f translates into e throughout the
-                # whole corpus
-                f_to_e_expect = f_to_e_prob * fe_count[(f_j, e_i)]
-
-                # expected number of times that f is used as the source of any
-                # translation
+    return (prob_prod, f_e_expect, f_source_expect)
