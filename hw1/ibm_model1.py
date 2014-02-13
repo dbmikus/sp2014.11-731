ee1f1b7155b8223609f6b64fdd8a224940af6172
Dylan Mikus <dbmikus@gmail.com>
2014-02-13 15:51:41 -0500

    IBM model 1 working, but it does not beat the baseline.

--- hw1/ibm_model1.py
+++ hw1/ibm_model1.py
@@ -20,9 +20,6 @@ def prepare_iters(bitext):
     f_source_count = defaultdict(int)
 
     for (f, e) in bitext:
-        # we clone the list so we don't have issues with extra null string
-        # insertions in the bitext
-        f = list(f)
         # We include the null token as an option for a word translation in the
         # source language.
         f.insert(0, '')
@@ -41,25 +38,6 @@ def prepare_iters(bitext):
     # Normalize the counts and use this as our starting probabilities
     # for p(e_i | f_j)
     return compute_pef_probs(f_e_count, f_source_count)
-
-
-
-def compute_pef_probs(f_e_expect, f_source_expect):
-    """
-    Computes all of the probabilities for translations: p(e_i | f_j)
-    """
-    p_e_f = defaultdict(int)
-    for ((f_j, e_i), trans_exp) in f_e_expect.items():
-        vprint('f_j = ' + f_j)
-        vprint('e_i = ' + e_i)
-        vprint('trans_exp = ' + str(trans_exp))
-        vprint('f_source_expect[f_j] = ' + str(f_source_expect[f_j]))
-        vprint('')
-        p_e_f[(f_j, e_i)] = float(trans_exp) /  float(f_source_expect[f_j])
-
-    vprint('done computing')
-    vprint(p_e_f)
-    return p_e_f
 
 
 def ibm_model1(bitext, me_iters):
@@ -93,12 +71,6 @@ def get_max_alignments(bitext, p_e_f):
 
 
 def get_max_sentence_alignment(f, e, p_e_f):
-    # we clone the list so we don't have issues with extra null string
-    # insertions in the bitext
-    f = list(f)
-    # add the null token as an empty string
-    f.insert(0, '')
-
     alignments = []
 
     # This allows a source word to translate to more than one target word,
@@ -115,9 +87,31 @@ def get_max_sentence_alignment(f, e, p_e_f):
         # if it is aligned to null string or there were no non-zero probability
         # alignments, then drop that alignment recording
         if (max_j is not None and max_j != 0):
-            alignments.append((max_j, i))
+            alignments.append((max_j-1, i))
 
     return alignments
+
+
+def compute_pef_probs(f_e_expect, f_source_expect):
+    """
+    Computes all of the probabilities for translations: p(e_i | f_j)
+    """
+    p_e_f = defaultdict(float)
+    for ((f_j, e_i), trans_exp) in f_e_expect.items():
+        vprint('f_j = ' + f_j)
+        vprint('e_i = ' + e_i)
+        vprint('trans_exp = ' + str(trans_exp))
+        vprint('f_source_expect[f_j] = ' + str(f_source_expect[f_j]))
+        vprint('')
+        p_e_f[(f_j, e_i)] = float(trans_exp) /  float(f_source_expect[f_j])
+
+    vprint('done computing')
+    vprint(p_e_f)
+    # print(f_e_expect[('Wiederaufnahme', 'Resumption')])
+    # print(f_source_expect['Wiederaufnahme'])
+    # print(p_e_f[('Wiederaufnahme', 'Resumption')])
+    # print ''
+    return p_e_f
 
 
 # me_iters is the number of ME iterations we do to improve our probabilities.
@@ -139,45 +133,30 @@ def run_iterations(bitext, me_iters):
         vprint("On iteration " + str(i))
         # The expected number of times we translate from a given word f_j to a
         # given word e_i.
-        f_e_expect = defaultdict(int)
-        f_source_expect = defaultdict(int)
+        f_e_expect = defaultdict(float)
+        f_source_expect = defaultdict(float)
+        # Total expected count of anything translating to e_i
+        total_e = defaultdict(float)
+        # We update the expected counts for the number of translations from f_j
+        # to e_i and the expected number of translations from f_j to any word in
+        # the target language.
         for (f, e) in bitext:
-            (f_e_expct, f_source_expct) = update_pair_counts(p_e_f, f, e)
+            for (i, e_i) in enumerate(e):
+                for (j, f_j) in enumerate(f):
+                    vprint('p_e_f[(' + f_j + ',' + e_i + ')] = ' + str(p_e_f[(f_j, e_i)]))
+                    # Computing the expected values that we use for our
+                    # iterative EM. These expected values are used to give us
+                    # the new p(e_i | f_j) probabilities at the end of each
+                    # iteration.
+                    total_e[e_i] += p_e_f[(f_j, e_i)]
 
-            # updating the expected values across the whole corpus
-            for ((f_j, e_i), trans_exp) in f_e_expct.items():
-                f_e_expect[(f_j, e_i)] += trans_exp
-            for (f_j, trans_exp) in f_source_expct.items():
-                f_source_expect[f_j] += trans_exp
+            for (j, f_j) in enumerate(f):
+                for (i, e_i) in enumerate(e):
+                    prob_trans = p_e_f[(f_j, e_i)]
+                    f_e_expect[(f_j, e_i)] += prob_trans / total_e[e_i]
+                    f_source_expect[f_j] += prob_trans / total_e[e_i]
 
         # Updating the probabilities for translations between words
         p_e_f = compute_pef_probs(f_e_expect, f_source_expect)
 
     return p_e_f
-
-
-# We update the expected counts for the number of translations from f_j to e_i
-# and the expected number of translations from f_j to any word in the target
-# language.
-def update_pair_counts(p_e_f, f, e):
-    # we clone the list so we don't have issues with extra null string
-    # insertions in the bitext
-    f = list(f)
-    # add the null token as an empty string
-    f.insert(0, '')
-
-    # Expected number of translations from some f_j to some e_i
-    f_e_expect = defaultdict(int)
-    f_source_expect = defaultdict(int)
-
-    for (j, f_j) in enumerate(f):
-        # for each i \in [1,2, ..., m]
-        for (i, e_i) in enumerate(e):
-            vprint('p_e_f[(' + f_j + ',' + e_i + ')] = ' + str(p_e_f[(f_j, e_i)]))
-            # Computing the expected values that we use for our iterative EM.
-            # These expected values are used to give us the new p(e_i | f_j)
-            # probabilities at the end of each iteration.
-            f_e_expect[(f_j, e_i)] += p_e_f[(f_j, e_i)]
-            f_source_expect[f_j] += p_e_f[(f_j, e_i)]
-
-    return (f_e_expect, f_source_expect)
