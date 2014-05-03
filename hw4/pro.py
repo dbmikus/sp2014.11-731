90e601b1e08a8e3f9571390c5d699db461412a57
Dylan Mikus <dbmikus@gmail.com>
2014-05-03 03:17:20 -0400

    Should now get the optimal weight vector

--- hw4/pro.py
+++ hw4/pro.py
@@ -2,8 +2,10 @@
 Pairwise ranking optimization.
 """
 
-import itertools, operator
+import sys
+import itertools, operator, math
 from random import randrange
+from sklearn import linear_model
 import bleu
 
 def single_bleu(hyp, ref):
@@ -27,9 +29,11 @@ def should_add(x):
 def sampler(hyps, ref, num_sample, num_ret):
     # Get all of the possible pairs between hypotheses, removing the ones that
     # are paired with themselves.
-    all_pairs = list(itertools.combinations(hyps, 2))
+    all_pairs = itertools.combinations(hyps, 2)
+    sys.stderr.write("Done generating combinations.\n")
     # Select num_sample random pairs and put them in a list.
     limited_pairs = (limit_shuffle(all_pairs, num_sample))[:num_sample]
+    sys.stderr.write("Done getting pairs from combinations.\n")
     # Compare the gold standard scores for the pairs
     pair_scores = []
     for pair in limited_pairs:
@@ -37,10 +41,13 @@ def sampler(hyps, ref, num_sample, num_ret):
         h2_score = single_bleu(pair[1][1], ref)
         if should_add(h1_score - h2_score):
             pair_scores.append((pair[0], pair[1], abs(h1_score - h2_score)))
+    sys.stderr.write("Done making list of golden score differences.\n")
     # Sort the list in descending order by the difference in the gold
     # standard score.
     pair_scores.sort(key=lambda x: -x[2])
-    ret_list = []
+    sys.stderr.write("Done sorting list by golden score.\n")
+    observed_vectors = []
+    targets = []
     # Take the top num_ret pair scores, preferring ones with the greatest gold
     # standard difference. For each pair score, compute the difference between
     # the feature vectors for the hypotheses in the pairs, and record whether
@@ -48,27 +55,31 @@ def sampler(hyps, ref, num_sample, num_ret):
     # element.
     for i in xrange(min(num_ret, len(pair_scores))):
         pair_score = pair_scores[i]
-        h1_gt_h2 = (single_bleu(pair_score[0][1], ref)
-                    > single_bleu(pair_score[1][1], ref))
+        h1_score = single_bleu(pair_score[0][1], ref)
+        h2_score = single_bleu(pair_score[1][1], ref)
+        gold_diff =  h1_score - h2_score
+        gold_diff_sign = math.copysign(1, gold_diff)
         # When computing the difference between feature vectors in a pair, we
         # are computing a vector from the second pair element to the first pair
         # element.
-        ret_list.append((vector_func_combine(operator.sub,
-                                             pair_score[0][2], pair_score[1][2]),
-                         h1_gt_h2))
+        observed_vectors.append(vector_func_combine(operator.sub,
+                                                    pair_score[0][2],
+                                                    pair_score[1][2]))
+        targets.append(gold_diff_sign)
         # Add the pair in the opposite direction.
         # When computing the difference between feature vectors in a pair, we
         # are computing a vector from the first pair element to the second pair
         # element.
-        ret_list.append((vector_func_combine(operator.sub,
-                                             pair_score[1][2], pair_score[0][2]),
-                         not h1_gt_h2))
-    return ret_list
+        observed_vectors.append(vector_func_combine(operator.sub,
+                                                    pair_score[1][2],
+                                                    pair_score[0][2]))
+        targets.append(-1.0 * gold_diff_sign)
+    sys.stderr.write("Done creating vector points and labels.\n")
+    return observed_vectors, targets
 
 
 def vector_func_combine(oper, v1, v2):
-    return [(kv_pair[0][0], oper(kv_pair[0][1], kv_pair[1][1]))
-            for kv_pair in zip(v1, v2)]
+    return [oper(vpair[0], vpair[1]) for vpair in zip(v1, v2)]
 
 
 # We randomly choose n elements in l and put them at the start of l.
@@ -82,10 +93,30 @@ def limit_shuffle(l, n):
     return l
 
 
+def train_classifier(observed_vectors, targets):
+    classifier = linear_model.Ridge(alpha=0.5)
+    classifier.fit(observed_vectors, targets)
+    return clf.coef_
+
+    # clf = linear_model.LogisticRegression()
+    # clf.fit(data[0],data[1])
+    # return clf.coef_
+
+    # #clf=linear_model.SGDRegressor(alpha='1.0')
+    # clf=linear_model.SGDRegressor()
+    # clf.fit(training_features,labels)
+    # return clf.predict(test_features)
+
+
 def main():
     all_hyps = [pair.split(' ||| ') for pair in open('data/dev.100best')]
     all_refs = [ref for ref in open('data/dev.ref')]
+    observed_vectors = []
+    targets = []
     num_sents = len(all_hyps) / 100
+    set_feat_names = False
+    feat_names = []
+    sys.stderr.write("Number of sentences = %d\n" % num_sents)
     for s in xrange(0, num_sents):
         hyps_for_one_sent = all_hyps[s * 100:s * 100 + 100]
         for i, hyp in enumerate(hyps_for_one_sent):
@@ -93,11 +124,20 @@ def main():
             split_feats = []
             for feat in feats.split(' '):
                 (k, v) = feat.split('=')
-                split_feats.append((k,float(v)))
+                if not set_feat_names:
+                    feat_names.append(k)
+                split_feats.append(float(v))
                 hyps_for_one_sent[i][2] = split_feats
+            set_feat_names = True
         ref = all_refs[s]
-        print sampler(hyps_for_one_sent, ref, 5000, 50)[2]
-        return
+        sys.stderr.write("Sampling from sentence %d...\n" % s)
+        more_obs_vecs, more_tgts = sampler(hyps_for_one_sent, ref, 5000, 50)
+        sys.stderr.write("Done sampling from sentence %d\n\n" % s)
+        observed_vectors += more_obs_vecs
+        targets += more_tgts
+    opt_weight_vec = train_classifier(observed_vectors, targets)
+    print opt_weight_vec
+
 
 if __name__ == '__main__':
     main()
